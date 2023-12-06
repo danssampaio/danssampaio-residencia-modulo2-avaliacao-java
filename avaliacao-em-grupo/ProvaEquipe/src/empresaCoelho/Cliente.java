00f44a79e3bfdb416425df2963160313acd5c989
Tiago Senger <tiagosengerds@gmail.com>
2023-12-06 20:23:03 -0300

    Update Classe Cliente

--- avaliacao-em-grupo/ProvaEquipe/src/empresaCoelho/Cliente.java
+++ avaliacao-em-grupo/ProvaEquipe/src/empresaCoelho/Cliente.java
@@ -13,24 +13,43 @@ public class Cliente {
 		this.imoveis = new ArrayList<>();
 	}
 
+    public String getNome() {
+        return nome;
+    }
+
+	
+	public String getCpf() {
+		return cpf;
+    }
+
+    public ArrayList<Imovel> getImoveis() {
+        return imoveis;
+    }
+
+
 	public void adicionarImovel(Imovel imovel) {
-		
+		imoveis.add(imovel);
 	}
 
 	public void removerImovel(Imovel imovel) {
-
+		imoveis.remove(imovel);
 	}
 	
 	public void listarImoveis() {
-
+		for (Imovel imovel : imoveis) {
+			System.out.println(imovel.toString());
+		}
 	}
-	
-	public void alterarCliente(String novoNome) {
-		
+
+	public void alterarCliente(String novoNome, String novoCpf) {
+		this.nome = novoNome;
+		this.cpf = novoCpf;
     }
 
 	public void gerarFatura() {
-		
-    }
-	
+		for (Imovel imovel : imoveis) {
+			Fatura fatura = imovel.realizarLeitura(imovel.getUltimaLeitura() + 10);
+			System.out.println(fatura.toString());
+		}
+    }	
 }
