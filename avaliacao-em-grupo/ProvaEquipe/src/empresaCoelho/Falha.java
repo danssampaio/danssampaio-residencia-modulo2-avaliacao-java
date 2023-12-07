e717cdc77f11b5ce30b82e47a70e6bdcea7ad3b0
girleide2 <147008816+girleide2@users.noreply.github.com>
2023-12-06 22:35:15 -0300

    Add files via upload
    Adicionando alterações em falha/falha distribuição/falha geração

--- avaliacao-em-grupo/ProvaEquipe/src/empresaCoelho/Falha.java
+++ avaliacao-em-grupo/ProvaEquipe/src/empresaCoelho/Falha.java
@@ -1,5 +1,8 @@
 package empresaCoelho;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Falha {
 	
@@ -21,7 +24,123 @@ public class Falha {
 		this.dataInicio = dataInicio;
 		this.resolvido = false;
 	}
+	
+	
+	 public static int getContadorFalhas() {
+		return contadorFalhas;
+	}
 
+
+	public static void setContadorFalhas(int contadorFalhas) {
+		Falha.contadorFalhas = contadorFalhas;
+	}
+
+
+	public int getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public String getPrevisao() {
+		return previsao;
+	}
+
+
+	public void setPrevisao(String previsao) {
+		this.previsao = previsao;
+	}
+
+
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+
+
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+
+	public LocalDate getDataFim() {
+		return dataFim;
+	}
+
+
+	public void setDataFim(LocalDate dataFim) {
+		this.dataFim = dataFim;
+	}
+
+
+	public Imovel getImovel() {
+		return imovel;
+	}
+
+
+	public void setImovel(Imovel imovel) {
+		this.imovel = imovel;
+	}
+
+
+	public static void incluirFalha(ArrayList<Falha> falhas, ArrayList<Imovel> imoveis) {
+	       
+			Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Digite o número de matrícula: ");
+	        int matricula = scanner.nextInt();
+
+	        Imovel imovel = imoveis.stream().filter(m -> m.getMatricula() == matricula).findFirst().orElse(null);
+
+	        if (imovel == null) {
+	            System.out.println("Imóvel não encontrado. Falha não registrada.");
+	            return;
+	        }
+
+	        scanner.nextLine();
+
+	        System.out.println("Digite a descrição da falha: ");
+	        String descricao = scanner.nextLine();
+
+	        System.out.println("Digite a previsão para resolução (DD/MM/AAAA): ");
+	        String previsao = scanner.nextLine();
+
+	        LocalDate dataInicio = LocalDate.now(); 
+
+	        System.out.println("Selecione o tipo de falha:");
+	        System.out.println("1. Falha de Geração");
+	        System.out.println("2. Falha de Distribuição");
+	        System.out.print("Escolha o tipo: ");
+
+	        int tipoFalha = scanner.nextInt();
+	        if (tipoFalha ==1) {
+	        	
+	        	FalhaGeracao novaFalha = new FalhaGeracao(imovel, descricao, previsao, dataInicio);
+	        	falhas.add(novaFalha);
+	        	
+	        } else if (tipoFalha ==2) {
+	        	
+	        	FalhaDistribuicao novaFalha = new FalhaDistribuicao(imovel, descricao, previsao, dataInicio);
+	        	falhas.add(novaFalha);
+	        }
+
+	        System.out.println("Falha registrada com sucesso!");
+	    }
+
+	
 	public boolean isResolvido() {
 		return resolvido;
 	}
@@ -29,8 +148,28 @@ public class Falha {
 	public void setResolvido(boolean resolvido) {
 		this.resolvido = resolvido;
 		if (resolvido) {
-			this.dataFim = LocalDate.now();
+		this.dataFim = LocalDate.now();
 		}
+	}
+	
+	private static void listarFalhasAberto(ArrayList<Falha> falhas){
+		System.out.println("\n---- LISTAR FALHAS EM ABERTO ----");
+
+        for (Falha falha : falhas) {
+            if (!falha.isResolvido()) {
+            	
+                System.out.println("Número de Falha: " + falha.getNumero());
+                System.out.println("Descrição: " + falha.getDescricao());
+                System.out.println("Previsão de Resolução: " + falha.getPrevisao());
+                System.out.println("Data de Início: " + falha.getDataInicio());
+                System.out.println("Imóvel: " + falha.getImovel().getMatricula());
+                System.out.println("Status: Aberto\n");
+            }
+        }
 	}
 
 }
+
+
+
+
--- avaliacao-em-grupo/ProvaEquipe/src/empresaCoelho/FalhaDistribuicao.java
+++ avaliacao-em-grupo/ProvaEquipe/src/empresaCoelho/FalhaDistribuicao.java
@@ -1,8 +1,12 @@
 package empresaCoelho;
-import java.time.LocalDate;
 
-class FalhaDistribuicao extends Falha {
-    public FalhaDistribuicao(Imovel imovel, String descricao, String previsao, LocalDate dataInicio) {
-        super(imovel, descricao, previsao, dataInicio);
-    }
+	class FalhaDistribuicao extends Falha {
+		
+		private Reparo reparo;
+		
+	    public FalhaDistribuicao(Imovel imovel, String descricao, String previsao, LocalDate dataInicio) {
+	        super(imovel, descricao, previsao, dataInicio);
+	        reparo = criaReparo();
+	    }
+	}
 }
